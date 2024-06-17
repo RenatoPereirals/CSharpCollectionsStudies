b8f859d61f6c6644ca0c1ad76891bd247c150d24
RenatoPereirals <renatopreirals@gmail.com>
2024-06-16 23:32:31 -0300

    Add Program.cs to demonstrate usage of ListStudies class

---
+++ Lists/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Exemplo de List<T>.");
+ListStudies program = new();
+
+// Adicionando elementos
+program.AddElement(10);
+program.AddElement(5);
+program.AddElement(8);
+
+// Adicionando uma coleção de elementos
+List<int> maisNumeros = [20, 15];
+program.AddRangeElements(maisNumeros);
+
+// Imprimindo os elementos
+Console.WriteLine("Elementos na lista:");
+program.PrintElements();
+
+// Verificando se um elemento está na lista
+int elementoProcurado = 5;
+bool encontrado = program.ContainsElement(elementoProcurado);
+Console.WriteLine($"O elemento {elementoProcurado} está na lista: {encontrado}");
+
+// Removendo um elemento da lista
+program.RemoveElement(8);
+
+// Imprimindo os elementos novamente após a remoção
+Console.WriteLine("Elementos na lista após remoção:");
+program.PrintElements();
+
+// Ordenando os elementos
+program.SortElements();
+
+// Imprimindo os elementos ordenados
+Console.WriteLine("Elementos na lista ordenados:");
+program.PrintElements();
